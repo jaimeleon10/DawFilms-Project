466f5cf1546a8a4ddbe2d00bc73c5b2e104cf232
Jaime León <jleonmulero@gmail.com>
2024-05-20 23:37:36 +0200

    fixed storage load test error by adding equals and hashcode to models

--- DawFilmsInterface/src/main/kotlin/org/example/dawfilmsinterface/productos/models/butacas/Butaca.kt
+++ DawFilmsInterface/src/main/kotlin/org/example/dawfilmsinterface/productos/models/butacas/Butaca.kt
@@ -20,4 +20,34 @@ class Butaca (
     override fun toString(): String {
         return "Butaca(id: $id, tipoButaca: $tipoButaca, imagen: $imagen, precio: ${tipoButaca.precio}, fila: $fila, columna: $columna, estadoButaca: $estadoButaca, ocupacionButaca: $ocupacionButaca, createdAt: $createdAt, updatedAt: $updatedAt, isDeleted: $isDeleted)"
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+
+        //Comparamos clase padre
+        if (!super.equals(other)) return false
+
+        //Tipamos other como Butaca para acceder a sus atributos
+        other as Butaca
+
+        //Comparamos atributos
+        if (fila != other.fila) return false
+        if (columna != other.columna) return false
+        if (tipoButaca != other.tipoButaca) return false
+        if (estadoButaca != other.estadoButaca) return false
+        if (ocupacionButaca != other.ocupacionButaca) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = super.hashCode()
+        //Se utiliza 31 porque es un número primero y es el menos probable de provocar errores
+        result = 31 * result + fila
+        result = 31 * result + columna
+        result = 31 * result + tipoButaca.hashCode()
+        result = 31 * result + estadoButaca.hashCode()
+        result = 31 * result + ocupacionButaca.hashCode()
+        return result
+    }
 }
