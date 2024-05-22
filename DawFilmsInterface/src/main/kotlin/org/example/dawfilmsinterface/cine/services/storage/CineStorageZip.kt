4d36ee8240009c4b6b84b0fc5fa9e9580e69d9f8
Jaime León <jleonmulero@gmail.com>
2024-05-22 15:51:48 +0200

    implemented backup of cinema with products and images, sales and clients

--- DawFilmsInterface/src/main/kotlin/org/example/dawfilmsinterface/cine/services/storage/CineStorageZip.kt
+++ DawFilmsInterface/src/main/kotlin/org/example/dawfilmsinterface/cine/services/storage/CineStorageZip.kt
@@ -8,6 +8,6 @@ import org.example.dawfilmsinterface.ventas.models.Venta
 import java.io.File
 
 interface CineStorageZip {
-    fun exportToZip(fileToZip: File, dataProducto: List<Producto>, dataCliente: List<Cliente>): Result<File, CineError> //dataVentas: List<Venta>
+    fun exportToZip(fileToZip: File, dataProductos: List<Producto>, dataClientes: List<Cliente>, dataVentas: List<Venta>): Result<File, CineError>
     fun loadFromZip(fileToUnzip: File): Result<List<Any>, CineError>
 }
