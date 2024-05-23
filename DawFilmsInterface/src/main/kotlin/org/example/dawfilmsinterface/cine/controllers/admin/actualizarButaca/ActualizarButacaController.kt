436b5629d4ba239e6f3ef67376bbcd546e393c3c
Natalia <nagonal2004@gmail.com>
2024-05-23 12:53:23 +0200

    cambio en view

--- DawFilmsInterface/src/main/kotlin/org/example/dawfilmsinterface/cine/controllers/admin/actualizarButaca/ActualizarButacaController.kt
+++ DawFilmsInterface/src/main/kotlin/org/example/dawfilmsinterface/cine/controllers/admin/actualizarButaca/ActualizarButacaController.kt
@@ -1,13 +1,19 @@
 package org.example.dawfilmsinterface.cine.controllers.admin.actualizarButaca
 
+import javafx.collections.FXCollections
 import javafx.fxml.FXML
 import javafx.scene.control.*
+import org.example.dawfilmsinterface.productos.viewmodels.ActualizarButacaViewModel
 import org.example.dawfilmsinterface.routes.RoutesManager
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 import org.lighthousegames.logging.logging
 
 private val logger = logging()
 
-class ActualizarButacaController {
+class ActualizarButacaController : KoinComponent {
+    val viewModel : ActualizarButacaViewModel by inject()
+
     @FXML
     lateinit var backMenuMenuButton: MenuItem
 
@@ -55,6 +61,50 @@ class ActualizarButacaController {
 
     @FXML
     private fun initialize() {
+        logger.debug { "Inicializando ActualizarButacaController FXML" }
+        initDefaultValues()
+
+        initBindings()
+
+        initEventos()
+
+
+    }
+
+    private fun initBindings() {
+        logger.debug { "Inicializando bindings"}
+
+        estadoSelectedField.textProperty().bind(viewModel.state.map { it.butaca.estado })
+        tipoSelectedField.textProperty().bind(viewModel.state.map { it.butaca.tipo})
+        ocupacionSelectedField.textProperty().bind(viewModel.state.map { it.butaca.ocupacion })
+        //precioSelectedField.textProperty().bind(viewModel.state.map { it.butaca.precio })
+
+        viewModel.state.addListener { _, _, newValue ->
+            logger.debug { "Actualizando datos de la vista" }
+            if (complementosTable.items != newValue.butacas){
+                complementosTable.items = FXCollections.observableArrayList(newValue.butacas)
+            }
+        }
+    }
+
+    private fun initDefaultValues() {
+        logger.debug { "Inicializando valores por defecto" }
+
+        tipoFilterComboBox.items = FXCollections.observableArrayList(viewModel.state.value.typesTipo)
+        tipoFilterComboBox.selectionModel.selectFirst()
+
+        estadoFilterComboBox.items = FXCollections.observableArrayList(viewModel.state.value.typesEstado)
+        estadoFilterComboBox.selectionModel.selectFirst()
+
+        ocupacionFilterComboBox.items = FXCollections.observableArrayList(viewModel.state.value.typesOcupacion)
+        estadoFilterComboBox.selectionModel.selectFirst()
+
+        complementosTable.items = FXCollections.observableArrayList(viewModel.state.value.butacas)
+
+
+    }
+
+    private fun initEventos() {
         acercaDeMenuButton.setOnAction { RoutesManager.initAcercaDeStage() }
         backMenuButton.setOnAction {
             logger.debug { "Cambiando de escena a ${RoutesManager.View.MENU_CINE_ADMIN}" }
