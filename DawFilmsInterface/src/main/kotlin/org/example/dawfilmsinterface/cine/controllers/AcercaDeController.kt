4ebb456395e1de3e25a39ffcd8d063892766bdc6
Javier Ruiz <javier.ruiz.serrano59@gmail.com>
2024-05-23 17:11:41 +0200

    gitflow-feature-stash: DocumentControllers
    Documentacion Completa de los Controllers

--- DawFilmsInterface/src/main/kotlin/org/example/dawfilmsinterface/cine/controllers/AcercaDeController.kt
+++ DawFilmsInterface/src/main/kotlin/org/example/dawfilmsinterface/cine/controllers/AcercaDeController.kt
@@ -9,6 +9,18 @@ import org.lighthousegames.logging.logging
 
 private val logger = logging()
 
+/**
+ * Clase controller para la ventana "Acerca de" a través de la IU.
+ * Gestiona las acciones y eventos relacionados con la visualización de información sobre los desarrolladores en la aplicación.
+ * @autor Jaime León, German Fernández, Natalia González, Alba García, Javier Ruiz
+ * @since 1.0.0
+ * @property exitAcercaDeButton Botón de menú para cerrar la ventana modal de Acerca de.
+ * @property githubAlbaLink Botón para abrir el perfil de GitHub de Alba García en el navegador.
+ * @property githubNataliaLink Botón para abrir el perfil de GitHub de Natalia González en el navegador.
+ * @property githubJaviLink Botón para abrir el perfil de GitHub de Javier Ruiz en el navegador.
+ * @property githubGermanLink Botón para abrir el perfil de GitHub de German Fernández en el navegador.
+ * @property githubJaimeLink Botón para abrir el perfil de GitHub de Jaime León en el navegador.
+ */
 class AcercaDeController {
     @FXML
     lateinit var exitAcercaDeButton: MenuItem
@@ -28,12 +40,20 @@ class AcercaDeController {
     @FXML
     lateinit var githubJaimeLink: Button
 
+    /**
+     * Establece la instancia de la ventana para controlar el cierre.
+     * @param stage La instancia de la ventana actual.
+     */
     // Utilizar viewModel cuando se implemente
     private lateinit var stage: Stage
     fun setStage(stage: Stage) {
         this.stage = stage
     }
 
+    /**
+     * Función que inicializa la vista de "Acerca de".
+     * Asigna las acciones a los botones y elementos de menú.
+     */
     @FXML
     private fun initialize(){
         exitAcercaDeButton.setOnAction {
@@ -62,6 +82,10 @@ class AcercaDeController {
         }
     }
 
+    /**
+     * Abre el navegador web en el enlace proporcionado.
+     * @param url La URL a la que se va a dirigir el navegador.
+     */
     private fun abrirNavegador(url: String) {
         logger.debug { "Abriendo navegador en el link: $url" }
         Open.open(url)
--- DawFilmsInterface/src/main/kotlin/org/example/dawfilmsinterface/cine/controllers/EstadoCineController.kt
+++ DawFilmsInterface/src/main/kotlin/org/example/dawfilmsinterface/cine/controllers/EstadoCineController.kt
@@ -9,6 +9,16 @@ import org.lighthousegames.logging.logging
 
 private val logger = logging()
 
+/**
+ * Clase controller para la visualización del estado del cine a través de la IU.
+ * Gestiona las acciones y eventos relacionados con la visualización del estado del cine en la aplicación.
+ * @autor Jaime León, German Fernández, Natalia González, Alba García, Javier Ruiz
+ * @since 1.0.0
+ * @property backMenuButton Botón para regresar al menú principal dependiendo del tipo de usuario.
+ * @property usernameField Etiqueta que muestra el nombre de usuario.
+ * @property acercaDeMenuButton Botón de menú para mostrar la información relevante de los desarrolladores.
+ * @property backMenuMenuButton Botón de menú para regresar al menú principal dependiendo del tipo de usuario.
+ */
 class EstadoCineController {
     @FXML
     lateinit var backMenuButton: Button
@@ -22,6 +32,10 @@ class EstadoCineController {
     @FXML
     lateinit var backMenuMenuButton: MenuItem
 
+    /**
+     * Función que inicializa la vista del estado del cine.
+     * Asigna las acciones al botón de regreso al menú principal y a los elementos de menú.
+     */
     @FXML
     private fun initialize() {
         backMenuButton.setOnAction {
