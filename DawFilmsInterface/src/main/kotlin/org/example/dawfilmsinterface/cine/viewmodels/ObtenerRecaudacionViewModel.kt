9faaa98163bf51e443dc8ed089b0f2c9346803a8
Natalia <nagonal2004@gmail.com>
2024-05-28 16:09:04 +0200

    cambio tabla recaudacion

--- DawFilmsInterface/src/main/kotlin/org/example/dawfilmsinterface/cine/viewmodels/ObtenerRecaudacionViewModel.kt
+++ DawFilmsInterface/src/main/kotlin/org/example/dawfilmsinterface/cine/viewmodels/ObtenerRecaudacionViewModel.kt
@@ -2,7 +2,6 @@ package org.example.dawfilmsinterface.cine.viewmodels
 
 import com.github.michaelbull.result.onSuccess
 import javafx.beans.property.SimpleObjectProperty
-import org.example.dawfilmsinterface.productos.models.producto.Producto
 import org.example.dawfilmsinterface.ventas.models.LineaVenta
 import org.example.dawfilmsinterface.ventas.services.VentaService
 import org.lighthousegames.logging.logging
---
+++ DawFilmsInterface/src/test/kotlin/org/example/dawfilmsinterface/cine/viewmodels/ObtenerRecaudacionViewModelTest.kt
@@ -0,0 +1,17 @@
+package org.example.dawfilmsinterface.cine.viewmodels
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class ObtenerRecaudacionViewModelTest {
+
+
+    @Test
+    fun getState() {
+    }
+
+    @Test
+    fun lineasFilteredList() {
+    }
+}
