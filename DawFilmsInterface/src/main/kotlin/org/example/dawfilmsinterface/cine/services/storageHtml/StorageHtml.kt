f771ec2a1ddef99b861aabba18d8d747cefd5873
Alba García Orduña <albag448@gmail.com>
2024-05-30 17:08:11 +0200

    gitflow-feature-stash: Informe-recaudacion
    Informe compra-venta con y sin iva

---
+++ DawFilmsInterface/src/main/kotlin/org/example/dawfilmsinterface/cine/services/storageHtml/StorageHtml.kt
@@ -0,0 +1,10 @@
+package org.example.dawfilmsinterface.cine.services.storageHtml
+
+import com.github.michaelbull.result.Result
+import org.example.dawfilmsinterface.ventas.errors.VentaError
+import org.example.dawfilmsinterface.ventas.models.LineaVenta
+import java.io.File
+
+interface StorageHtml {
+    fun exportHtml(listaLineas: List<LineaVenta>, file: File): Result<Unit, VentaError>
+}
