d0e93e99ce9b55690b80e758733884243262e4fb
Jaime León <jleonmulero@gmail.com>
2024-05-31 12:09:20 +0200

    gitflow-feature-stash: fix-storage-Img-test

--- DawFilmsInterface/src/test/kotlin/org/example/dawfilmsinterface/productos/storage/genericStorage/ProductosStorageImplTest.kt
+++ DawFilmsInterface/src/test/kotlin/org/example/dawfilmsinterface/productos/storage/genericStorage/ProductosStorageImplTest.kt
@@ -6,15 +6,10 @@ import org.example.dawfilmsinterface.productos.models.complementos.CategoriaComp
 import org.example.dawfilmsinterface.productos.models.complementos.Complemento
 import org.example.dawfilmsinterface.productos.models.producto.Producto
 import org.example.dawfilmsinterface.productos.storage.storageCsv.StorageCsv
-import org.example.dawfilmsinterface.productos.storage.storageCsv.StorageCsvImpl
 import org.example.dawfilmsinterface.productos.storage.storageImage.StorageImage
 import org.example.dawfilmsinterface.productos.storage.storageImage.StorageImageImpl
 import org.example.dawfilmsinterface.productos.storage.storageJson.StorageJson
-import org.example.dawfilmsinterface.productos.storage.storageJson.StorageJsonImpl
 import org.example.dawfilmsinterface.productos.storage.storageXml.StorageXml
-import org.example.dawfilmsinterface.productos.storage.storageXml.StorageXmlImpl
-import org.example.dawfilmsinterface.productos.storage.storageZip.StorageZip
-import org.example.dawfilmsinterface.productos.storage.storageZip.StorageZipImpl
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
 
@@ -50,15 +45,11 @@ class ProductosStorageImplTest {
     private lateinit var myFileXml: File
 
     @Mock
-    private lateinit var storageZip: StorageZip
-    private lateinit var myFileZip: File
-
-    @Mock
     private lateinit var storageImage: StorageImage
     private lateinit var myFileImg: File
 
     @Mock
-    private lateinit var config:Config
+    private lateinit var config: Config
 
     @InjectMocks
     private lateinit var storage: ProductosStorageImpl
@@ -70,14 +61,12 @@ class ProductosStorageImplTest {
 
     @BeforeEach
     fun setUp() {
+        config = Config()
         myFileCsv = Files.createTempFile("productos", ".csv").toFile()
         myFileJson = Files.createTempFile("productos", ".json").toFile()
         myFileXml = Files.createTempFile("productos", ".xml").toFile()
-        storageImage = StorageImageImpl(Config())
-        storageZip = StorageZipImpl(Config(), StorageJsonImpl())
-        myFileZip = Files.createTempFile("productos", ".zip").toFile()
+        storageImage = StorageImageImpl(config)
         myFileImg = Files.createTempFile("image", ".png").toFile()
-        config = Config()
     }
 
     @AfterEach
@@ -85,7 +74,7 @@ class ProductosStorageImplTest {
         Files.deleteIfExists(myFileCsv.toPath())
         Files.deleteIfExists(myFileJson.toPath())
         Files.deleteIfExists(myFileXml.toPath())
-        Files.deleteIfExists(myFileZip.toPath())
+        Files.deleteIfExists(myFileImg.toPath())
     }
 
     @Order(1)
@@ -191,16 +180,12 @@ class ProductosStorageImplTest {
 
     @Test
     fun saveImage() {
-
+        val storageImage = StorageImageImpl(config)
 
         val result = storageImage.saveImage(myFileImg)
 
-        whenever(storageImpl.getImageName(myFileImg)).thenReturn(myFileImg.name)
-        whenever(storageImage.saveImage(myFileImg)).thenReturn(Ok(myFileImg))
+        assertTrue { result.isOk }
 
-        val img = storage.saveImage(myFileImg)
-
-        assertTrue { img.isOk }
         assertEquals("png", result.value.extension)
 
         Files.deleteIfExists(result.value.toPath())
@@ -242,39 +227,5 @@ class ProductosStorageImplTest {
         assertTrue(result.isOk)
 
         Files.deleteIfExists(result.value.toPath())
-    }
-
-    @Order(1)
-    @Test
-    fun exportToZip() {
-        val data = listOf<Producto>(
-            Complemento("1", "Complemento", "futura_imagen.png", "Palomitas", 3.0, 20, CategoriaComplemento.COMIDA, LocalDate.now(), LocalDate.now(),false)
-        )
-
-        whenever(storageZip.exportToZip(myFileZip, data)).thenReturn(Ok(myFileZip))
-
-        val zip = storage.exportToZip(myFileZip, data)
-
-        assertTrue { zip.isOk }
-        assertEquals(data.size.toLong(), zip.value)
-        assertEquals("zip", zip.value.extension)
-        assertTrue(zip.value.isFile)
-        //No podemos verificar el nombre debido a que cambia cada vez que genera un temporal nuevo
-    }
-
-    @Order(2)
-    @Test
-    fun loadFromZip() {
-        val data = listOf<Producto>(
-            Complemento("1", "Complemento", "futura_imagen.png", "Palomitas", 3.0, 20, CategoriaComplemento.COMIDA, LocalDate.now(), LocalDate.now(),false)
-        )
-
-        storageZip.exportToZip(myFileZip, data)
-        println("Datos exportados en fichero Zip: $myFileZip")
-
-        val result = storageZip.loadFromZip(myFileZip)
-
-        assertTrue(result.isOk)
-        assertEquals(data, result.value)
     }
 }
